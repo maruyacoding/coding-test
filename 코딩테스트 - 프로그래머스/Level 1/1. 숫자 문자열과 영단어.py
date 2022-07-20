e1cc4b1981f53351edc3bee3385e11a76223109b
0108sally <97514461+maruyacoding@users.noreply.github.com>
2022-07-20 22:57:06 +0900

    Delete 1. 숫자 문자열과 영단어.py

--- 코딩테스트 - 프로그래머스/Level 1/1. 숫자 문자열과 영단어.py
+++
@@ -1,7 +0,0 @@
-def solution(s):
-    eng = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-    # enumerate : 인덱스 값을 포함하는 enumerate 객체를 돌려준다.
-    for idx,num in enumerate(eng) :
-        if num in s :
-            s = s.replace(num, str(idx))
-    return int(s)
