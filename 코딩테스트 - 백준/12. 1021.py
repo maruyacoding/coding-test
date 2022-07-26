6c07489042fa124016d59c5cc65315f6f1363301
0108sally <97514461+maruyacoding@users.noreply.github.com>
2022-07-26 20:44:22 +0900

    20220726

--- 코딩테스트 - 백준/12. 1021.py
+++ 코딩테스트 - 백준/12. 1021.py
@@ -22,3 +22,17 @@ for target in targets :
     d.popleft() # 원소 꺼내기
     
 print(cnt)
+
+
+# 다른 사람 코드
+n, m = map(int, input().split(' '))
+dq = [i for i in range(1, n + 1)]
+
+ans = 0
+
+for find in map(int, input().split(' ')) :
+    ix = dq.index(find)
+    ans += min(len(dq[ix : ]), len(dq[: ix]))
+    dq = dq[ix + 1 :] + dq[: ix]
+
+print(ans)
