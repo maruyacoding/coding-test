b77f1bcbfa7e928131ccafad204c498104119863
0108sally <97514461+maruyacoding@users.noreply.github.com>
2022-07-26 22:42:47 +0900

    20220726

--- 코딩테스트 - 프로그래머스/Level 1/37. 로또의 최고 순위와 최저 순위.py
+++ 코딩테스트 - 프로그래머스/Level 1/37. 로또의 최고 순위와 최저 순위.py
@@ -15,4 +15,14 @@ def solution(lottos, win_nums):
     
     return [best_rank, worst_rank]
     
-            
+# 다른 사람 코드
+def solution(lottos, win_nums):
+
+    rank=[6,6,5,4,3,2,1]
+
+    cnt_0 = lottos.count(0)
+    ans = 0
+    for x in win_nums:
+        if x in lottos:
+            ans += 1
+    return rank[cnt_0 + ans],rank[ans]
